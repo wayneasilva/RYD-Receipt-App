3e0f80098cb500a9da26c6a0116b42fdc9758950
wayneasilva <wayneasilva@gmail.com>
2018-11-16 17:17:29 -0500

    functins up until buildReceipt functioning

--- app.js
+++ app.js
@@ -15,16 +15,16 @@ const receiptSchema = new mongoose.Schema({
     lastName: String,
     phone: String,
     saleDate:{type: Date, default: Date.now},
-    item1: {name: String, price: Number},
-    item2: {name: String, price: Number},
-    item3: {name: String, price: Number},
-    item4: {name: String, price: Number},
-    item5: {name: String, price: Number},
-    item6: {name: String, price: Number},
-    item7: {name: String, price: Number},
-    item8: {name: String, price: Number},
-    item9: {name: String, price: Number},
-    item10: {name: String, price: Number},
+    item1: {name: String, price: Number, tax: Number, Total: Number},
+    item2: {name: String, price: Number, tax: Number, Total: Number},
+    item3: {name: String, price: Number, tax: Number, Total: Number},
+    item4: {name: String, price: Number, tax: Number, Total: Number},
+    item5: {name: String, price: Number, tax: Number, Total: Number},
+    item6: {name: String, price: Number, tax: Number, Total: Number},
+    item7: {name: String, price: Number, tax: Number, Total: Number},
+    item8: {name: String, price: Number, tax: Number, Total: Number},
+    item9: {name: String, price: Number, tax: Number, Total: Number},
+    item10: {name: String, price: Number, tax: Number, Total: Number},
     notes: String
 });
 
@@ -58,23 +58,97 @@ app.get("/receipts", (req, res) => {
 
 // //NEW ROUTE
 app.get("/receipts/new", (req, res) => {res.render("new");})
+//must calculate totals and include tax etc and submit to server**
 
 // //CREATE ROUTE
 app.post("/receipts", (req, res) => {
     // req.body.receipt.body = req.sanitize(req.receipt.blog.body);
-    Receipt.create(req.body.receipt, (err, newReceipt) => {
-        if (err) {
-            res.render("new");
-        }
-        else {
-            res.redirect("/receipts");
-        }
-    })
+    let saleData = req.body.receipt;
+    let taxRate = 0.075;
+     
+    let pricesPreTax = [];
+    let pricesPreTaxConv = [];
+    let taxedPrices = [];
+    let subTotal = 0;
+    
+    //This line pushes our 10 item prices into an array.
+    pricesPreTax.push(
+        saleData['item1']['price'], 
+        saleData['item2']['price'], 
+        saleData['item3']['price'], 
+        saleData['item4']['price'], 
+        saleData['item5']['price'], 
+        saleData['item6']['price'], 
+        saleData['item7']['price'], 
+        saleData['item8']['price'], 
+        saleData['item9']['price'], 
+        saleData['item10']['price']
+    );
+
+
+    
+    //This function will convert our pricePreTax array values to float from string
+    function convertToFloat(pricesPreTax) {
+        pricesPreTax.forEach((price) => {
+            if (price !== '') {
+                let convertedPrice = parseFloat(price);
+                pricesPreTaxConv.push(convertedPrice);
+            }
+        })
+    }
+    
+    //This function will take our converted pricePreTaxConv array vales and add them to our subTotal variable
+    function calculateSubtotal(pricesPreTaxConv) {
+        pricesPreTaxConv.forEach((price) => {
+            subTotal += price;
+        })
+        console.log(subTotal);
+    }
+
+    //This function will return an array with each element being the item price + tax.
+    function calculateItemPlusTax(pricesPreTaxConv) {
+        pricesPreTaxConv.forEach((price) => {
+            let tax = price * taxRate;
+            taxedPrices.push(price + tax);
+            console.log(taxedPrices);
+        })
+    }
+
+    function buildReceipt() {
+        let finalSaleReceipt = {};
+
+        finalSaleReceipt['receiptNumber'] = saleData['receiptNumber'];
+        finalSaleReceipt['firstName'] = saleData['firstName'];
+        finalSaleReceipt['lastName'] = saleData['lastName'];
+    }
+
+    convertToFloat(pricesPreTax);
+    calculateSubtotal(pricesPreTaxConv);
+    calculateItemPlusTax(pricesPreTaxConv);
+    console.log(finalSaleReceipt);
+
+
+
+
+    // console.log(req);
+    // console.log("########################")
+    // console.log(req.body);
+    // console.log("########################")
+    // console.log(req.body.receipt);
+    // console.log("########################")
+    // Receipt.create(req.body.receipt, (err, newReceipt) => {
+    //     if (err) {
+    //         res.render("new");
+    //     }
+    //     else {
+    //         res.redirect("/receipts");
+    //     }
+    // })
 })
 
 //SHOW ROUTE
-app.get("/receipts/:id", function(req, res) {
-    Receipt.findById(req.params.id, function(err, foundReceipt) {
+app.get("/receipts/:id", (req, res) => {
+    Receipt.findById(req.params.id, (err, foundReceipt) => {
         if (err) {
             // res.redirect("/receipts");
         }
@@ -84,17 +158,17 @@ app.get("/receipts/:id", function(req, res) {
     })
 })
 
-// //EDIT ROUTE
-// app.get("/blogs/:id/edit", function(req, res) {
-//     Blog.findById(req.params.id, function(err, foundBlog) {
-//         if (err) {
-//             res.redirect("/blogs");
-//         }
-//         else {
-//             res.render("edit", {blog: foundBlog});
-//         }
-//     })
-// })
+//EDIT ROUTE
+app.get("/receipts/:id/edit", (req, res) => {
+    Receipt.findById(req.params.id, (err, foundReceipt) => {
+        if (err) {
+            res.redirect("/receipts");
+        }
+        else {
+            res.render("edit", {receipt: foundReceipt});
+        }
+    })
+})
 
 // //UPDATE ROUTE
 // app.put("/blogs/:id", function(req, res) {
